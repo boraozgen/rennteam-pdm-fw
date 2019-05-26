fd431544bca6502e1fc7373668017477ccede7af
Bora Özgen <bora.oezgen@rennteam-stuttgart.de>
2019-05-26 21:10:47 +0200

    Set actual fuse values for motor start

--- firmware/Inc/App.h
+++ firmware/Inc/App.h
@@ -21,6 +21,22 @@
 #define CURRENT_FRAME_INTERVAL_MS	1000
 #define FUSE_TASK_INTERVAL_MS		1
 
+#define CH1_FUSE_CURRENT		15.0F	// Water Fan L
+#define CH2_FUSE_CURRENT		15.0F	// Water Fan R
+#define CH3_FUSE_CURRENT		10.0F	// Water Pump L+R
+#define CH4_FUSE_CURRENT		15.0F	// Servos
+#define CH5_FUSE_CURRENT		20.0F	// Ignition + Injection
+#define CH6_FUSE_CURRENT		5.0F	// LTC
+#define CH7_FUSE_CURRENT		5.0F	// ECU
+#define CH8_FUSE_CURRENT		5.0F	// Sensors 12V
+#define CH9_FUSE_CURRENT		10.0F	// Oil Fan
+#define CH10_FUSE_CURRENT		5.0F	// Not used
+#define CH11_FUSE_CURRENT		5.0F	// Not used
+#define CH12_FUSE_CURRENT		5.0F	// Not used
+#define CH13_FUSE_CURRENT		5.0F	// Not used
+#define CH14_FUSE_CURRENT		5.0F	// Not used
+#define CH15_FUSE_CURRENT		5.0F	// Not used
+
 /* Enumeration to map channels to n-1 for optimal array usage */
 enum {
 	CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, CH13, CH14, CH15
